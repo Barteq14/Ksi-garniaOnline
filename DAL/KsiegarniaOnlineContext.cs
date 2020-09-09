7389d98c70a8c54651ec43f12d3ca680231033a6
Bartek Longota <b.longota2@wp.pl>
2020-09-09 14:07:00 +0200

    add

---
+++ DAL/KsiegarniaOnlineContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KsiegarniaOnline.DAL
+{
+    public class KsiegarniaOnlineContext : DbContext
+    {
+        public KsiegarniaOnlineContext(DbContextOptions<KsiegarniaOnlineContext> options)
+         : base(options)
+        {
+        }
+
+
+    }
+}
